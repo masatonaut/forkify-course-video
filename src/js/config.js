d530a9ad4bdec41a2e96f163625e37287467d868
masatonaut <masato-ito@cybozu.co.jp>
2022-03-21 10:41:14 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1e493072-68f6-4523-9ca1-d27c24e84b5c';
+export const MODAL_CLOSE_SEC = 2.5;
